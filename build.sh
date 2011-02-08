e589db2b8561c50b7b1a9dc2e8e98eeae8058afe
xotic750 <xotic750@02f5f032-bb5d-a9c2-f8bf-6e674d3f6dfa>
2011-02-08 16:07:02 +0000

    First commit

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# -----------------------------------
+# Please copy this file as "build.sh"
+# Edit to put in the correct paths for your system
+
+VER="0.1.0"
+echo "Creating release version $VER"
+sed s/!version!/$VER/g utility.js > utility-$VER.js
+echo "Done."
+
+# --------------------------------------------------------------------------------------
+# MINIMISED VERSION - This will fail on errors so use is advised - required for release!
+# Change path to compiler and source - obtain it from here:
+# http://code.google.com/closure/compiler/
+
+echo "Creating minimised release (will also show errors)"
+java -jar bin/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --output_wrapper "(function(){%output%})();" --js utility-$VER.js > utility-$VER.min.js
+echo "Done."
