dd86e5be502bc5a40b82ae654f40bd54786de5d9
xotic750 <xotic750@02f5f032-bb5d-a9c2-f8bf-6e674d3f6dfa>
2011-07-12 01:04:05 +0000

    v0.2.3 Fixed Storage Helper compression Added Common DOM Messaging Helper

--- trunk/build.sh
+++ trunk/build.sh
@@ -3,7 +3,7 @@
 # Please copy this file as "build.sh"
 # Edit to put in the correct paths for your system
 
-VER="0.2.2"
+VER="0.2.3"
 echo "Creating release version $VER"
 sed s/!version!/$VER/g head.js > utility-$VER.js
 sed s/!version!/$VER/g utility.js >> utility-$VER.js
